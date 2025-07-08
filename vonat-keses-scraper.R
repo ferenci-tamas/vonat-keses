31dc33d7c9fcdaea5f84563d291488cff8a4dde7
Tamás Ferenci <tamas.ferenci@medstat.hu>
2025-07-08 09:47:44 +0200

    Hibakezelés javítása.

--- vonat-keses-scraper.R
+++ vonat-keses-scraper.R
@@ -40,13 +40,16 @@ res <- lapply(1:maxv, function(v) {
   pg <- purrr::insistently(function() rvest::read_html(paste0(
     elviraurl, v, "&d=", datum, "&ed=", EDszam)),
     rate = purrr::rate_delay(pause = 2, max_times = 10))()
-  tab <- rvest::html_table(pg, header = FALSE)[[1]]
-  tab <- if(tab$X1[3] == "Km") tab[-c(1, 3),] else tab[-2,]
-  cbind(setNames(tab[-1,], make.names(as.character(tab[1,]),
-                                      unique = TRUE)),
-        Datum = as.Date(datum, format = "%y.%m.%d"), Vonat = v,
-        VonatSzam = rvest::html_text(rvest::html_nodes(
-          pg, xpath = "//div[@id='tul']/h2")))
+  tab <- rvest::html_table(pg, header = FALSE)
+  if(length(tab) == 0) return(NULL) else {
+    tab <- tab[[1]]
+    tab <- if(tab$X1[3] == "Km") tab[-c(1, 3),] else tab[-2,]
+    cbind(setNames(tab[-1,], make.names(as.character(tab[1,]),
+                                        unique = TRUE)),
+          Datum = as.Date(datum, format = "%y.%m.%d"), Vonat = v,
+          VonatSzam = rvest::html_text(rvest::html_nodes(
+            pg, xpath = "//div[@id='tul']/h2")))
+  }
 })
 
 saveRDS(res, paste0(

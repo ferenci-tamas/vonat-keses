40452ddc34991ec0d7f41e505930af811b264efa
Tamás Ferenci <tamas.ferenci@medstat.hu>
2026-06-24 01:11:18 +0200

    Javítás.

--- debug.R
+++ debug.R
@@ -1,3 +1,8 @@
 EDszam <- rvest::read_html("https://elvira.mav-start.hu/", encoding = "ISO-8859-1")
 EDszam <- rvest::html_text(rvest::html_nodes(EDszam, "script"))
+EDszam <- sapply(EDszam, function(s)
+  sub("ed:'([0-9A-F]+)'", "\\1",
+      regmatches(s, regexpr("ed:'[0-9A-F]+'", s))))
+EDszam <- EDszam[[which(sapply(EDszam, length) > 0)]]
+
 print(EDszam)
